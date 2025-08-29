9c598e2f42bc54a2a602e3accd80d2b7d2b2720a
bgrass2721 <benjamin.grass@eleve.ensai.fr>
2025-08-29 14:44:07 +0000

    création du fichier py pour tester l'api

---
+++ test.py
@@ -0,0 +1,27 @@
+from lrclib import LrcLibAPI
+
+# Crée une instance de l'API (il est recommandé de préciser un user_agent)
+api = LrcLibAPI(user_agent="mon-app/1.0")
+
+# Récupère les paroles d'une chanson
+lyrics = api.get_lyrics(
+    track_name="I Want to Live",
+    artist_name="Borislav Slavov",
+    album_name="Baldur's Gate 3 (Original Game Soundtrack)",
+    duration=233,  # durée en secondes (optionnel mais recommandé)
+)
+
+# Affiche les paroles synchronisées ou non
+found_lyrics = lyrics.synced_lyrics or lyrics.plain_lyrics
+
+# Recherche des paroles
+results = api.search_lyrics(track_name="Life Letters")
+
+# Affiche les 5 premiers résultats
+for result in results[:5]:
+    print(f"{result.artist_name} - {result.track_name} ({result.album_name})")
+
+# Récupère les paroles du premier résultat
+lyrics = api.get_lyrics_by_id(results[0].id)
+found_lyrics = lyrics.synced_lyrics or lyrics.plain_lyrics
+print(found_lyrics)
